1e8e7bb1f1863cc0dedc2a489b0ee52499ec5c14
Nithish S <150901506+NithishProgrammer@users.noreply.github.com>
2026-03-12 20:29:13 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+import random
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+f = open("quotes.txt" , 'r')
+r = f.readlines()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],   # allow all websites
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/quote")
+def get_quote():
+    return {"Quote": random.choice(r)}
